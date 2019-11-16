b22d24898faa4dfafe4d9bc0f75c078b6147704a
Shamel Lakin <christlife4u@gmail.com>
2019-11-16 22:36:39 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class CreateHauntedHouses < ActiveRecord::Migration[5.0]
   def change
     create_table :haunted_houses do |t|
@@ -12,3 +13,20 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.0]
     end
   end
 end
+=======
+class CreateHauntedHouse < ActiveRecord::Migration[5.0]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.text :long_description
+    end
+  end
+end
+# Create your haunted_houses migration here
+>>>>>>> 3b32deb1d9c747287499db0dc7c1997de1b05ad7
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 3) do
 
   create_table "costume_stores", force: :cascade do |t|
@@ -41,5 +42,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "closing_date"
     t.text "description"
   end
+=======
+ActiveRecord::Schema.define(version: 0) do
+>>>>>>> 3b32deb1d9c747287499db0dc7c1997de1b05ad7
 
 end
